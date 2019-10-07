032d3e90f886b626ef3fcfa09dcabddd4b5363cf
Manuel <github@mrspoocy.com>
2019-10-07 13:07:15 +0200

    add TypeScript-Definition files update concat script

---
+++ Function.d.ts
@@ -0,0 +1,27 @@
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+
+/**
+ * Creates a new function.
+ */
+interface Function {
+    /**
+     * For a given function, creates a bound function that has the same body as the original function.
+     * The this object of the bound function is associated with the specified object, and has the specified initial parameters.
+     * @param thisArg An object to which the this keyword can refer inside the new function.
+     * @param argArray A list of arguments to be passed to the new function.
+     */
+    bind(this: Function, thisArg: any, ...argArray: any[]): any;
+}
--- bin/concat.js
+++ bin/concat.js
@@ -1,27 +1,55 @@
 const folders = ['./Array/', './Function/', './JSON/', './Object/', './String/', './Date/'];
-// const testFolder = './Array/';
 const fs = require('fs');
 const path = require('path');
 const cat = require('cat');
 const bundlePath = path.resolve(process.cwd(), './index.js');
+const bundleTypeScriptDefinitionPath = path.resolve(process.cwd(), './index.d.ts');
+let hasLicense = false;
 
-// clear the file
-fs.writeFileSync(path.resolve(process.cwd(), bundlePath), '');
+// clear the files (if not exists, it will be created)
+fs.writeFileSync(bundlePath, '');
+fs.writeFileSync(bundleTypeScriptDefinitionPath, '');
+
 // loop folders
 folders.forEach(folder => {
-  fs.readdir(folder, (err, files) => {
-    files = files.filter(file => !(/(^|\/)\.[^\/\.]/g).test(file))
-    files.forEach(file => {
-      // console.log(file);
-      let filePath = path.resolve(process.cwd(), `${folder}${file}`);
-      // console.log(filePath);
-      cat(filePath, (error, data)=>{
-        if(error !== null) {
-          console.log(error);
-          process.exit();
-        }
-        fs.appendFileSync(bundlePath, `//${file}\n${data}\n`);
-      });
+
+    fs.readdir(folder, (err, files) => {
+        files = files.filter(file => !(/(^|\/)\.[^\/\.]/g).test(file));
+        files.forEach(file => {
+            let filePath = path.resolve(process.cwd(), `${folder}${file}`);
+            cat(filePath, (error, data) => {
+                if (error !== null) {
+                    console.log(error);
+                    process.exit();
+                }
+                fs.appendFileSync(bundlePath, `//${file}\n${data}\n`);
+            });
+        });
+
     });
-  });
+
+    // build TypeScript-Definition path
+    const tsdFilename = `${path.basename(folder)}.d.ts`;
+    const tsdPath = path.resolve(process.cwd(), tsdFilename);
+
+    // if TypeScript-Definition exists
+    if (fs.existsSync(tsdPath)) {
+        cat(tsdPath, (error, data) => {
+            if (error !== null) {
+                console.log(error);
+                process.exit();
+            }
+
+
+            if (!hasLicense) {
+                // add one times Apache license
+                hasLicense = true;
+            } else {
+                // remove Apache License
+                data = data.split('\n').slice(14).join('\n');
+            }
+
+            fs.appendFileSync(bundleTypeScriptDefinitionPath, `// ${tsdFilename}\n${data}\n`);
+        });
+    }
 });
