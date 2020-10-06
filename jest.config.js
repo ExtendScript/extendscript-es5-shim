c3eacc35770491bf619cdb49035bba37b57df84e
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-10-06 07:41:03 +0200

    test setup stashing

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: "jest-environment-node",
   collectCoverage: true,
   coverageReporters: ["lcov", "text"],
+  testPathIgnorePatterns: ["<rootDir>/__tests__/assertions.js"],
   collectCoverageFrom: [
     "Array/**/*.{js}",
     "Date/**/*.{js}",
